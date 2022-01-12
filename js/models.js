9c10c99dc0ca3d71bb4cd923375f7da641a871f4
Oliveiralsdo <oliveira.lsdo@gmail.com>
2022-01-12 18:02:51 -0300

    Projeto com JavaScript

---
+++ js/models.js
@@ -0,0 +1,11 @@
+let modelsJson = [
+    {id:1, name:'Tyrion Lannister', img:'img/tyrionl.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Tyrion Lannister'},
+    {id:2, name:'Cersei Lannister', img:'img/cerseil.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Cersei Lannister'},
+    {id:3, name:'Jon Snow', img:'img/jons.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Jon Snow'},
+    {id:4, name:'Sansa Stark', img:'img/sansa1.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Sansa Stark'},
+    {id:5, name:'Khal Drogo', img:'img/khald.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Khal Drogo'},
+    {id:6, name:'Drogon', img:'img/drogon.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Drogon'},
+    {id:7, name:'Daenerys Targaryen', img:'img/daeneryst.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Daenerys Targaryen'},
+    {id:8, name:'Night King', img:'img/whitew.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Night King'},
+    {id:9, name:'Tormund Giantsbane', img:'img/tormundg.jpg', price:[100.00, 150.00, 200.00], sizes:['', '', ''], description:'FUNKO POP - GAME OF THRONES:  Tormund Giantsbane'}
+];
